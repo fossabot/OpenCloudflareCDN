d6ee970514d4e39e0164588b13a9270b1125e33e
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-08-12 18:51:56 +0800

    fix(frontend-util): ipv4/v6 display bug
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- Frontend/src/util/cloudflare.ts
+++ Frontend/src/util/cloudflare.ts
@@ -3,8 +3,10 @@ export const genRayID = () => {
 }
 
 export const getRootDomain = () => {
-    const parts = window.location.hostname.split('.');
-    return parts.length > 2
-        ? parts.slice(-2).join('.')
-        : window.location.hostname;
+    const host = window.location.hostname;
+    const isIPv4 = /^(\d{1,3}\.){3}\d{1,3}$/.test(host);
+    const isIPv6 = /^\[?([a-fA-F0-9:]+)\]?$/.test(host);
+    if (isIPv4 || isIPv6) return host;
+    const parts = host.split('.');
+    return parts.length > 2 ? parts.slice(-2).join('.') : host;
 }
