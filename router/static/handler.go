f9b14781091e853ea134f8bb667f620a690f45a6
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-08-21 03:56:44 +0800

    feat(static): auto contentType & staticRoot 403 http status code
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- router/static/handler.go
+++ router/static/handler.go
@@ -1,7 +1,12 @@
 package static
 
 import (
+	"errors"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/Sn0wo2/OpenCloudflareCDN/config"
 	"github.com/Sn0wo2/OpenCloudflareCDN/internal/util"
@@ -12,23 +17,57 @@ import (
 
 func Handle() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		if ctx.Request.Method == http.MethodGet {
-			staticRoot := config.Instance.StaticPath
-			if staticRoot == "" {
-				ctx.Next()
-
-				return
-			}
-
-			filePath := GetStaticFile(config.Instance.StaticIndex, staticRoot, ctx.Request.URL.Path)
-			if filePath == "" {
-				ctx.Next()
-
-				return
-			}
-
-			log.Instance.Info("S >> Static file served", zap.String("file", filePath), zap.String("ctx", util.GinContextString(ctx)))
-			ctx.File(filePath)
+		if ctx.Request.Method != http.MethodGet {
+			ctx.Next()
+			return
 		}
+
+		staticRoot := config.Instance.StaticPath
+		if staticRoot == "" {
+			_ = ctx.Error(errors.New("static path is empty"))
+			ctx.Next()
+			return
+		}
+
+		status := http.StatusOK
+
+		if absStaticRoot, err := filepath.Abs(staticRoot); err == nil {
+			if absTargetPath, err := filepath.Abs(filepath.Join(staticRoot, strings.TrimPrefix(ctx.Request.URL.Path, "/"))); err == nil {
+				if absStaticRoot == absTargetPath {
+					status = http.StatusForbidden
+				}
+			}
+		}
+
+		filePath := GetStaticFile(config.Instance.StaticIndex, staticRoot, ctx.Request.URL.Path)
+		if filePath == "" {
+			_ = ctx.Error(errors.New("static file not found"))
+			ctx.Next()
+			return
+		}
+
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			_ = ctx.Error(err)
+			ctx.Next()
+			return
+		}
+
+		contentType := mime.TypeByExtension(filepath.Ext(filePath))
+		if contentType == "" {
+			contentType = "application/octet-stream"
+		}
+		if strings.HasPrefix(contentType, "text/") {
+			contentType += "; charset=utf-8"
+		}
+
+		fileContent, err := os.ReadFile(filePath)
+		if err != nil {
+			ctx.Next()
+			return
+		}
+
+		log.Instance.Info("S >> Static file served", zap.String("file", filePath), zap.String("ctx", util.GinContextString(ctx)))
+
+		ctx.Data(status, contentType, fileContent)
 	}
 }
