7b89721380d98dab557a320509a461c0a48e3de5
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-08-22 03:22:21 +0800

    chore(vite): compress index.html
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react'
 import * as path from "path";
 import {defineConfig} from 'vite'
+import {createHtmlPlugin} from 'vite-plugin-html'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        createHtmlPlugin({
+            minify: true
+        })
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src")
