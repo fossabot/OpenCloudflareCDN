234286f9799764e12b38660e18649f45f170e3b6
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-08-22 23:34:05 +0800

    fix(frontend-api): url typo
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- Frontend/src/service/api.ts
+++ Frontend/src/service/api.ts
@@ -9,7 +9,7 @@ async function safeJsonParse<T>(res: globalThis.Response): Promise<T | null> {
     }
 }
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/oofc-api/';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/ocfc-api/';
 
 export const Verify = async (
     payload: VerifyPayload
