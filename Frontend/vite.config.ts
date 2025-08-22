cd47c00be5c2b216f8d3abcfa6dab876d79057a9
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-08-23 00:34:08 +0800

    fix(frontend-vite): build outDir
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -15,5 +15,9 @@ export default defineConfig({
         alias: {
             "@": path.resolve(__dirname, "./src")
         }
-    }
+    },
+    build: {
+        outDir: './static',
+        emptyOutDir: true,
+    },
 })
